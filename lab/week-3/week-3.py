842d52e6cd26f6be2fca38b1b74578c752694ba1
mordecaimaic <110681224+mordecaimaic@users.noreply.github.com>
2023-11-09 19:01:38 +0800

    Update week-3.py

--- lab/week-3/week-3.py
+++ lab/week-3/week-3.py
@@ -173,3 +173,59 @@ def sum_square(x):
     while n <= x:
         total, n = total + square(n), n + 1
     return total
+
+
+#Q7: Doge
+"""记录一下这个函数，看了有一点久
+much可以理解为是一个修饰器（也就是对函数进行修饰的函数）
+但是在理解为什么much == wow可以对函数进行判断的之前，我们可以先假设一个例子，可以做出一个验证
+1. 证明
+证明函数体内部可使可以读取到当前函数frame的名字的
+def test():
+    print("Debug", test)
+当运行test()的时候，我们可以看到Debug <Function...>
+当运行test的时候，我们看到<Function...>，和上面是一样的，因此可以说明在函数frame内部的时候，是可以读取到当前frame的函数地址的
+2.因此当当执行much(much)的时候，我们可以知道 if much == wow这个条件很明显是正确的
+3.在函数内部重新定义一个wow，这个函数不需要参数。并且当这个函数被执行的时候[wow()]，会返回一个新的函数such
+4.such需要接受一个参数，但是such函数的主体和和传入的参数没有关系，会直接返回5
+5.所以much(much)将会返回一个函数的修饰器，我们暂且把这个称做为lambda_1，并且它不会被执行。
+6.接下来将会进入到much(lambda_1)，此时if much == wow这个条件为假。
+7.接下来将会执行such = lambda wow: 4，但是这条语句很明显没有起到任何作用
+8.所以such(lambda_1)将会直接返回wow()。wow也就是函数修饰器lambda_1，在执行函数修饰器lambda_1之后，将会生成一个新的函数
+9.这个函数也就是我们在执行much(much)的时候定义的such，我们可以把这个函数先叫做lambda_2。lambda_2需要接受一个参数，但是直接返回5
+10.所以 such(lambda_1) = lambda2
+11.lambda2(Anything) = 5,结束:)
+
+"""
+wow = 6
+
+def much(wow):
+    if much == wow:
+        such = lambda wow: 5
+        print("Same")
+        def wow():
+            return such
+        return wow
+    such = lambda wow: 4
+    return wow()
+
+
+# wow = much(much(much))(wow)
+
+def test():
+    print("Debug", test)
+
+
+y = "y"
+h = y
+def y(y):
+    h = "h"
+    if y == h:
+        return y + "i"
+    y = lambda y: y(h)
+    return lambda h: y(h)
+y = y(y)(y)
+
+
+
+
